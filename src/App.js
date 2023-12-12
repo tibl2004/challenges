89de0c3caede8b787ce42100d8b639269427577d
tibl2004 <timo.blumer@gewa.ch>
2023-12-12 20:25:00 +0100

    challenge4s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [storedData, setStoredData] = useState([]);
+
+  useEffect(() => {
+    // Daten aus dem localStorage beim Laden der Seite abrufen
+    const storedDataFromLocalStorage = JSON.parse(localStorage.getItem('data')) || [];
+    setStoredData(storedDataFromLocalStorage);
+  }, []);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Neue Daten zum bestehenden Array hinzufügen
+    const newData = [...storedData, inputValue];
+    setStoredData(newData);
+
+    // Daten im localStorage speichern
+    localStorage.setItem('data', JSON.stringify(newData));
+
+    // Input-Feld zurücksetzen
+    setInputValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Challenges</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Daten eingeben:
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Absenden</button>
+      </form>
+      <h2>Gespeicherte Daten:</h2>
+      <ul>
+        {storedData.map((data, index) => (
+          <li key={index}>{data}</li>
+        ))}
+      </ul>
     </div>
   );
 }
